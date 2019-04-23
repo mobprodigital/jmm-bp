288177f342f8130b16888bb50aca071b1f54f707
sunil <48079759+sunilkumar321@users.noreply.github.com>
2019-04-23 15:43:53 +0530

    country flag method commit

--- adserver/adserve/application/controllers/login.php
+++ adserver/adserve/application/controllers/login.php
@@ -172,6 +172,18 @@ class Login extends CI_Controller{
 		$this->load->view('publisher/login');	
 	}
 	
+	function getFlag(){
+		$isd_code = (trim($_GET["isocode"]) <> "" ) ? trim($_GET["isocode"]) : "";
+		$data['cat']			= 'inventory';
+		$data['activeaction']	= 'viewuser';
+		$data['country'] = $this->Login_Model->getFlagVal($isd_code);
+		echo json_encode($data['country']);
+		//echo "<pre>";print_r($data); die;
+			 
+		 
+	}
+
+
 	function publisherLogout(){
 		$this->session->sess_destroy();
 		redirect('publisher/login');
